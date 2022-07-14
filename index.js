4c225c4d112129b40f892c1aa4da8009fec25f1a
JustEddie <skdpwls830@gmail.com>
2022-07-14 17:53:43 +1000

    Done.

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ function distanceTravelledInFeet(block1, block2) {
 }
 
 function calculatesFarePrice(start, destination) {
-    let distance = distanceTravelledInFeet(start, destination);
+    const distance = distanceTravelledInFeet(start, destination);
+    let price = 0;
+
     if (distance > 2500) {
         return 'cannot travel that far';
-    } else if (2000 < distance <= 2500) {
-        console.log(distance);
+    } else if (2000 < distance && distance <= 2500) {
         return 25;
-    } else if (400 < distance <= 2000) {
+    } else if (400 < distance && distance <= 2000) {
+        console.log(distance);
         return 0.02*(distance - 400);
     } else {
         return 0;
